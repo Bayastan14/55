9a09a10ce6953d095433c2a3591db6888d9f2274
nadezhda <matievbaastan8@gmail.com>
2022-03-03 18:50:52 +0600

    first commit

---
+++ src/com/company/Boss.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class Boss {
+    private int health;
+    private int damage;
+    private int defence;
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public void setDefence(int defence) {
+        this.defence = defence;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getDefence() {
+        return defence;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Boss boss = new Boss();
+        boss.setHealth(500);
+        boss.setDamage(100);
+        boss.setDefence(120);
+        System.out.println("Boss won !!!");
+        System.out.println("Boss lose !!!");
+
+
+
+
+    }
+}
